e4efc36bff1e38f7c9e89cc4c201b603776ab12a
chew01 <chewjh19@gmail.com>
2021-12-22 23:00:38 +0800

    ship and gameboard factories added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,65 @@
-console.log("Hello World!");
+const Ship = (length) => {
+  const hitArray = [];
+  const hit = (number) => {
+    if (!hitArray.includes(number)) {
+      hitArray.push(number);
+    }
+  };
+  const isSunk = () => {
+    if (hitArray.length === length) {
+      return true;
+    }
+    return false;
+  };
+
+  return { length, hit, isSunk };
+};
+
+const Gameboard = () => {
+  const board = [];
+  for (let i = 0; i < 10; i += 1) {
+    board.push([]);
+  }
+
+  const placeShip = (xCoord, yCoord, length, orientation) => {
+    const newShip = Ship(length);
+    if (orientation === 'vertical') {
+      for (let i = yCoord; i <= yCoord + length; i += 1) {
+        board[xCoord][i] = newShip;
+      }
+    } else {
+      for (let i = xCoord; i <= xCoord + length; i += 1) {
+        board[i][yCoord] = newShip;
+      }
+    }
+  };
+
+  const missedShots = [];
+
+  const receiveAttack = (xCoord, yCoord) => {
+    const gridHit = board[xCoord][yCoord];
+    if (gridHit === undefined) {
+      missedShots.push([xCoord, yCoord]);
+    } else {
+      gridHit.hit();
+    }
+  };
+
+  const isAllSunk = () => {
+    let check = false;
+    for (let i = 0; i < 10; i += 1) {
+      for (let n = 0; n < 10; n += 1) {
+        const ship = board[i][n];
+        if (ship !== undefined && ship.isSunk()) {
+          check = true;
+        }
+      }
+    }
+
+    return check;
+  };
+
+  return { board, placeShip, missedShots, receiveAttack, isAllSunk };
+};
+
+export { Ship, Gameboard };
---
+++ src/index.test.js
@@ -0,0 +1,65 @@
+import { Ship, Gameboard } from './index';
+
+test('ship factory creates an object with length property', () => {
+  const testShip = Ship(4);
+  expect(testShip.length).toBe(4);
+});
+
+test('ship factory creates an object with working hit function and sunk function', () => {
+  const testShip = Ship(4);
+  expect(typeof testShip.hit).toBe('function');
+
+  testShip.hit(1);
+  testShip.hit(2);
+  testShip.hit(3);
+  expect(testShip.isSunk()).toBe(false);
+
+  testShip.hit(4);
+  expect(testShip.isSunk()).toBe(true);
+});
+
+test('receiveAttack function sends hit function upon successful hit', () => {
+  const gameboard = Gameboard();
+  gameboard.placeShip(2, 2, 2, 'horizontal');
+
+  const testShip = gameboard.board[3][2];
+  testShip.hit = jest.fn();
+  gameboard.receiveAttack(2, 2);
+  expect(testShip.hit).toHaveBeenCalled();
+});
+
+test('receiveAttack function updates missedShots upon failed hit', () => {
+  const gameboard = Gameboard();
+  gameboard.placeShip(1, 2, 1, 'horizontal');
+
+  gameboard.receiveAttack(3, 2);
+  expect(gameboard.missedShots).toContainEqual([3, 2]);
+});
+
+test('isAllSunk function returns true when all ships are sunk', () => {
+  const gameboard = Gameboard();
+  gameboard.placeShip(3, 3, 1, 'horizontal');
+  gameboard.placeShip(4, 2, 2, 'vertical');
+
+  gameboard.receiveAttack(3, 3);
+  gameboard.receiveAttack(4, 2);
+  gameboard.receiveAttack(4, 3);
+  expect(gameboard.isAllSunk()).toBe(true);
+});
+
+test('isAllSunk function returns false when no ships are sunk', () => {
+  const gameboard = Gameboard();
+  gameboard.placeShip(5, 3, 1, 'horizontal');
+  gameboard.placeShip(4, 5, 2, 'vertical');
+
+  expect(gameboard.isAllSunk()).toBe(false);
+});
+
+test('isAllSunk function returns false when only some ships are sunk', () => {
+  const gameboard = Gameboard();
+  gameboard.placeShip(5, 3, 1, 'horizontal');
+  gameboard.placeShip(4, 5, 2, 'vertical');
+  gameboard.placeShip(1, 4, 1, 'vertical');
+
+  expect(gameboard.isAllSunk()).toBe(false);
+});
